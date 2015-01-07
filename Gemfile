d19ef8a2233bb6949e5f008e27573538d6dec5af
Craig Little <craiglttl@gmail.com>
2015-01-06 16:29:52 -0800

    Add Code Climate test reporter configuration

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gemspec
 group :development do
   gem 'bump', '~> 0.1', require: false
 end
+
+group :test do
+  gem 'codeclimate-test-reporter', '~> 0.4', require: false
+end
--- spec/helper.rb
+++ spec/helper.rb
@@ -1,3 +1,9 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+
+  CodeClimate::TestReporter.start
+end
+
 require 'biz'
 
 RSpec.configure do |config|
