198c2012ffcb44c031a4544b63e38ea23df28d7d
jules Ivanic <jules.ivanic@gmail.com>
2018-09-21 14:26:52 +0200

    Change `BIG_BANG` and `HEAT_DEATH` values to JRuby compatible values

--- lib/biz/time.rb
+++ lib/biz/time.rb
@@ -14,8 +14,8 @@ module Biz
     DAY_MINUTES  = DAY_HOURS * HOUR_MINUTES
     WEEK_MINUTES = WEEK_DAYS * DAY_MINUTES
 
-    BIG_BANG   = ::Time.new(-10**100).freeze
-    HEAT_DEATH = ::Time.new(10**100).freeze
+    BIG_BANG   = ::Time.new(-4712, 1, 1, Date::ITALY).freeze
+    HEAT_DEATH = ::Time.new(4712, 1, 1, Date::ITALY).freeze
 
     private_constant :MINUTE_SECONDS,
                      :HOUR_MINUTES,
