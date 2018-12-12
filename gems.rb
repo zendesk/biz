052dbaa42e0179c46e7709a0bd1f0b5a0c677b20
Craig Little <craiglttl@gmail.com>
2018-12-12 12:11:47 -0800

    Upgrade to `benchmark-ipsa` for memory stats
    We can essentially drop in `benchmark-ipsa` to start getting memory
    statistics in our benchmarks. Seems good.
    
    Example:
    ```
    Benchmark: Return time 100 business hours after origin
    Allocations -------------------------------------
                     biz  1286/190  alloc/ret        7/1  strings/ret
           business_time    3763/3  alloc/ret        7/0  strings/ret
           working_hours   1151/10  alloc/ret        2/0  strings/ret
    Warming up --------------------------------------
                     biz    99.000  i/100ms
           business_time    43.000  i/100ms
           working_hours    95.000  i/100ms
    Calculating -------------------------------------
                     biz      1.006k (± 3.2%) i/s -      5.049k
           business_time    440.021  (± 2.5%) i/s -      2.236k
           working_hours    965.186  (± 2.3%) i/s -      4.845k
    
    Comparison:
                     biz:     1005.9 i/s
           working_hours:      965.2 i/s - same-ish: difference falls within error
           business_time:      440.0 i/s - 2.29x slower
    ```

--- gems.rb
+++ gems.rb
@@ -5,9 +5,9 @@ source 'https://rubygems.org'
 gemspec
 
 group :benchmark do
-  gem 'benchmark-ips', '~> 2.0',   require: false
-  gem 'business_time', '~> 0.9.0', require: false
-  gem 'working_hours', '~> 1.0',   require: false
+  gem 'benchmark-ipsa', '~> 0.2.0', require: false
+  gem 'business_time',  '~> 0.9.0', require: false
+  gem 'working_hours',  '~> 1.0',   require: false
 end
 
 group :ci do
